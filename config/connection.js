d63d846f8203c0a67ec6da731b0f64f5f8453847
jesterb0206 <jesterb@seattleu.edu>
2022-11-15 15:47:00 -0800

    Created connection.js and configured the server

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+// If a JawsDB url exists connect to a Heroku server, otherwise connect to your localhost server
+
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql',
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+    });
+
+module.exports = sequelize;
