3af9c641001348a6174aa5ac14fdbf5c4e08385d
jesterb0206 <jesterb@seattleu.edu>
2022-11-17 13:12:01 -0800

    homepage.js initial commit

---
+++ public/js/homepage.js
@@ -0,0 +1,19 @@
+const title = document.querySelector("#post-title");
+
+const viewPost = async (event) => {
+  event.stopPropagation();
+  const id = event.target.getAttribute("data-id");
+
+  const response = await fetch(`/post/${id}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/viewpost");
+  } else {
+    console.log("Unable to view post!");
+  }
+};
+
+title.addEventListener("click", viewPost);
