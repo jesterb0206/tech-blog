060478de881cb3c4ac58a71603ad63b2f7e3e475
jesterb0206 <jesterb@seattleu.edu>
2022-11-17 14:23:01 -0800

    signup.js initial commit

---
+++ public/js/signup.js
@@ -0,0 +1,24 @@
+const signUpFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/user/signup", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      console.log("Unable to create an account!");
+    }
+  }
+};
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signUpFormHandler);
