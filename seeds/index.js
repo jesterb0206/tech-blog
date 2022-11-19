bbab5c88f8eac103e92d45de9b8f7d1f799f8d14
jesterb0206 <jesterb@seattleu.edu>
2022-11-18 17:13:01 -0800

    seeds/index.js initial commit

---
+++ seeds/index.js
@@ -0,0 +1,18 @@
+const sequelize = require("../config/connection");
+const seedUser = require("./userData");
+const seedPost = require("./postData");
+const seedComment = require("./commentData");
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedUser();
+
+  await seedPost();
+
+  await seedComment();
+
+  process.exit(0);
+};
+
+seedAll();
