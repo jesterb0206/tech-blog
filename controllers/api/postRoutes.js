7bc3c7ebad8f3f517f3b075af57993090b807fff
jesterb0206 <jesterb@seattleu.edu>
2023-01-10 16:07:02 -0800

    Fixed An Error in postRoutes.js That Was Preventing A User From Being Taken Back To An Updated Dashboard Upon Deleting A Post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -48,6 +48,12 @@ router.delete('/:id', async (req, res) => {
         id: req.params.id,
       },
     });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
     res.status(200).json(postData);
   } catch (err) {
     console.log(err);
